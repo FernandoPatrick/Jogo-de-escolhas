ebc7f5ffe437c9d4640b343e335f44b316159d40
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-04-21 11:57:11 -0400

    update

--- pages/Inicio/inicio1.js
+++ pages/Inicio/inicio1.js
@@ -4,18 +4,34 @@ $('html').hide()
 alert('Chegando na cena do crime, o policial responsável pede suas credenciais.')
 $('html').fadeIn(1000)
 
-setTimeout(function() {
-  var nome = prompt('"Identifique-se detetive!"')
-  while (true) {
-    if(nome != '' || nome.length() <= 1 ){
-    alert(`Muito bem Detetive ${nome}, vamos ao caso:`)
-    break;
-  }
-   else {
+setTimeout(function validar() {
+  var nome = prompt('Identifique-se detetive!')
+  while (nome.length() <= 1) {
       alert('Credenciais inválidas!')
-    nome = prompt('Qual seu nome Detetive!?')
+      nome = prompt('Qual seu nome Detetive!?')
     }
-  }
+  while (isNaN(nome)) {
+      alert('Credenciais inválidas!')
+      nome = prompt('Qual seu nome Detetive!?')
+    }
+  while (nome == '') {
+      alert('Credenciais inválidas!')
+      nome = prompt('Qual seu nome Detetive!?')
+    }
+    return alert(`Muito bem Detetive ${nome}, vamos ao caso:`)
+  //   else if(isNaN(nome)) {
+  //     alert('Credenciais inválidas!')
+  //     nome = prompt('Qual seu nome Detetive!?')
+  //   }
+  // //   else if(nome != ''){
+  // //   alert(`Muito bem Detetive ${nome}, vamos ao caso:`)
+  // //   break;
+  // // }
+  //   else {
+  //     alert('Credenciais inválidas!')
+  //     nome = prompt('Qual seu nome Detetive!?')
+  //   }
+  // }
 }, 1000)
 
 

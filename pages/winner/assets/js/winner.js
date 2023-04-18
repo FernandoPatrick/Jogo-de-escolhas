924ca13f0c703985c4bbbe8fb7d6acc8eae33221
Fernando Patrick <fernando_lopes27@hotmail.com>
2023-04-18 20:37:50 -0300

    Update winner.js

--- pages/winner/assets/js/winner.js
+++ pages/winner/assets/js/winner.js
@@ -17,18 +17,4 @@ function comemorar() {
     }
     img.setAttribute('src', images[i])
 
-
-  // if(img.getAttribute('src') == './img/IMG1.png'){
-  //   img.setAttribute('src', './img/img3.png') 
-  //  }else if(img.getAttribute('src') == './img/img3.png'){
-  //   img.setAttribute('src', './img/img4.png') 
-  //  }else if(img.getAttribute('src') == './img/img4.png'){
-  //   img.setAttribute('src', './img/img6.png') 
-  //   }
-  //  else if(img.getAttribute('src') == './img/img6.png'){
-  //   img.setAttribute('src', './img/img5.png') 
-  //   }
-  //  else if(img.getAttribute('src') == './img/img5.png'){
-  //   img.setAttribute('src', './img/img3.png') 
-  //   }
 }
